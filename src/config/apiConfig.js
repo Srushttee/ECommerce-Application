1816fb9da4d24bb9320b54baca66baf37995b60d
Srushttee <srushtitatte66@gmail.com>
2025-09-16 16:34:30 +0530

    url changed

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://ecommerce-backend-production-93fd.up.railway.app"
+    ? "https://ecommerce-backend-production-3d6b.up.railway.app"
     : "http://localhost:8080";
 
 const api = axios.create({
